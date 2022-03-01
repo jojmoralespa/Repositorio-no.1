4e358d28680317986f1c7fc9e1783921beaac91f
Johan <jojmoralespa@unal.edu.co>
2022-03-01 14:04:24 -0500

    Cambios en punto 6.2

--- miscelanea_1/ejercicio_6.2.py
+++ miscelanea_1/ejercicio_6.2.py
@@ -1,24 +1,43 @@
 
 #input data
 pay_list = []
+hour_list = []
 
-i = 1
-total = 0
+count = 1
+total_pay = 0
+total_h = 0
+
 try:
-    while i <= 3:
-        hours_str = input(f"Enter Hours worked in day {i}: ")
-        hour_value_str = input(f"Enter Hour value in day {i}: $ ")
+    while count <= 3:
+        hours_str = input(f"Enter Hours worked in day {count}: ")
+        hour_value_str = input(f"Enter Hour value in day {count}: $ ")
 
         hours = float(hours_str)
         hour_value = float(hour_value_str)
+        hour_list.append(hours)
 
         #Amount to pay
         pay = hours*hour_value
         pay_list.append(pay)
-        i = i + 1
+        count = count + 1
     for value in pay_list:
-        total = total + value
+        total_pay = total_pay + value
+    #Total hours worked    
+    for value in hour_list:
+        total_h = total_h + value
 
-    print(f"You must to pay : $ {round(total,2)}")
+    #Results
+    print("")
+    print("Results: ")
+    print("")
+    print(f"You must to pay : $ {round(total_pay,2)}")
+    print(f"Total hours worked : {round(total_h,2)} h")
+
+    # amount to pay each day
+    count = 1
+    for value in pay_list:
+        print(f"amount to pay in day {count}: $ {round(value,2)}")
+        count += 1
+
 except:
     print("Data error")
